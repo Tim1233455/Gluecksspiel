73a04a3636ee7e499aeb77c971d46101cb78b93c
Tim1233455 <tim.kaufmann04@gmail.com>
2022-05-12 12:52:13 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let ergebnis = x1 + x2;
+let min = 1;
+let max = 6;
+let x1 = Math.round(Math.random() * (max - min)) + min;
+let x2 = Math.round(Math.random() * (max - min)) + min;
+let ergebnis1 = x1 + x2;
+let ergebnis2 = x1 + x2;
+let oberergebnis = ergebnis + x1 + x2;
+
+function wuerfeln (){
+
+     x1 = Math.round(Math.random() * (max - min)) + min;
+     x2 = Math.round(Math.random() * (max - min)) + min;
+     ergebnis1 = x1 + x2;
+}
+
+if (ergebnis == 7){ oberergebnis - 7
+
+}
+
+function anzahlwuerfe1 () {
+
+
+}
